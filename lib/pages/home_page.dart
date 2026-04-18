3e761cd1ef7742a494641ea401e9f99fe0cb65d7
Niek Geerligs <geerligsniek@gmail.com>
2026-04-18 12:39:04 +0200

    added membership hardcoded

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:viertoregym/pages/membership_page.dart';
+import 'package:viertoregym/pages/studios_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.user});
@@ -11,6 +15,170 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset("assets/Icon.png", width: 120),
+        ),
+        title: Text(
+          "VierToreGym",
+          style: TextStyle(
+            color: Color(0xff3e9f1f),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            spacing: 12,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hi, ${widget.user}",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(
+                    width: Get.width * .3,
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.white),
+                        shape: WidgetStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(32),
+                            side: BorderSide(
+                              color: Color(0xff3fa220),
+                              width: 3,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: Get.back,
+                      child: Text(
+                        "Sign Out",
+                        style: TextStyle(
+                          color: Color(0xff3fa220),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              InkWell(
+                onTap: () {
+                  Get.to(() => MembershipPage(user: widget.user));
+                },
+                child: SizedBox(
+                  width: double.maxFinite,
+                  height: Get.height * .3,
+                  child: Card(
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(32),
+                    ),
+                    child: Stack(
+                      clipBehavior: Clip.hardEdge,
+                      children: [
+                        Positioned(
+                          right: -240,
+                          top: -150,
+                          child: Container(
+                            width: 450,
+                            height: 450,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xffddeddb),
+                            ),
+                          ),
+                        ),
+                        Positioned.fill(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            spacing: 12,
+                            children: [
+                              Image.asset(
+                                "assets/Icons/User attributes.png",
+                                height: 100,
+                              ),
+                              Text(
+                                "Membership",
+                                style: TextStyle(
+                                  color: Color(0xff3e9f1f),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 32,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Get.to(() => StudiosPage());
+                },
+                child: SizedBox(
+                  width: double.maxFinite,
+                  height: Get.height * .3,
+                  child: Card(
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(32),
+                    ),
+                    child: Stack(
+                      clipBehavior: Clip.hardEdge,
+                      children: [
+                        Positioned(
+                          right: -240,
+                          top: -150,
+                          child: Container(
+                            width: 450,
+                            height: 450,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xffddeddb),
+                            ),
+                          ),
+                        ),
+                        Positioned.fill(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            spacing: 12,
+                            children: [
+                              Image.asset(
+                                "assets/Icons/Location on.png",
+                                height: 100,
+                              ),
+                              Text(
+                                "Studios",
+                                style: TextStyle(
+                                  color: Color(0xff3e9f1f),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 32,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/membership_page.dart
+++ lib/pages/membership_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/state_manager.dart';
 
 class MembershipPage extends StatefulWidget {
-  const MembershipPage({super.key});
-
+  const MembershipPage({super.key, required this.user});
+  final String user;
   @override
   State<MembershipPage> createState() => _MembershipPageState();
 }
@@ -10,6 +12,199 @@ class MembershipPage extends StatefulWidget {
 class _MembershipPageState extends State<MembershipPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var outlineInputBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(32),
+      borderSide: BorderSide(color: Color(0xff3fa220), width: 3),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset("assets/Icon.png", width: 120),
+        ),
+        title: Text(
+          "Membership",
+          style: TextStyle(
+            color: Color(0xff3e9f1f),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            spacing: 24,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: double.maxFinite,
+                height: Get.height * .35,
+                child: Card(
+                  elevation: 8,
+                  color: Color(0xffe3f0e1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(32),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Member Details",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text("Save"),
+                              style: ButtonStyle(
+                                backgroundColor: WidgetStatePropertyAll(
+                                  Color(0xff3fa220),
+                                ),
+                                foregroundColor: WidgetStatePropertyAll(
+                                  Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        TextFormField(
+                          onChanged: (_) {
+                            setState(() {});
+                          },
+                          validator: (value) {
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hint: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              spacing: 12,
+                              children: [
+                                Icon(Icons.person),
+                                Text(
+                                  "${widget.user}",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: outlineInputBorder,
+                            focusedBorder: outlineInputBorder,
+                            border: outlineInputBorder,
+                          ),
+                        ),
+                        TextFormField(
+                          onChanged: (_) {
+                            setState(() {});
+                          },
+                          validator: (value) {
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hint: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              spacing: 12,
+                              children: [
+                                Icon(Icons.person),
+                                Text(
+                                  "Weinstein",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            enabledBorder: outlineInputBorder,
+                            focusedBorder: outlineInputBorder,
+                            border: outlineInputBorder,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () => Get.dialog(
+                            CalendarDatePicker(
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                              onDateChanged: (value) {
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          child: TextFormField(
+                            enabled: false,
+                            onChanged: (_) {
+                              setState(() {});
+                            },
+                            validator: (value) {
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              hint: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                spacing: 12,
+                                children: [
+                                  Icon(Icons.calendar_month),
+                                  Text(
+                                    "June 7, 1983",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                              filled: true,
+                              fillColor: Colors.white,
+                              enabledBorder: outlineInputBorder,
+                              focusedBorder: outlineInputBorder,
+                              border: outlineInputBorder,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.maxFinite,
+                height: Get.height * .2,
+                child: Card(
+                  elevation: 8,
+                  color: Color(0xffe3f0e1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(32),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Subscription Details",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text("Bronze Subscription"),
+                        Text("Expires on June 29, 2025"),
+                        Text("20.00€/month"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
