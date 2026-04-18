2cb1b6494504a4e4f2be4767afcbc31be13975f5
Niek Geerligs <geerligsniek@gmail.com>
2026-04-18 11:47:25 +0200

    added json reading

--- lib/services/json_reader.dart
+++ lib/services/json_reader.dart
@@ -1,1 +1,11 @@
-class JsonReader {}
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class JsonReader {
+  static Future<dynamic> readJson(String path) async {
+    final json = await rootBundle.loadString(path);
+    final data = await jsonDecode(json);
+    return data;
+  }
+}
